52627d3f0a702494f1b6f1148f197fe500843ec6
Sharad Regoti <sharadregoti15@gmail.com>
2025-05-05 17:31:32 +0530

    Fixes

---
+++ public/images/kubernetes-services-explained:cluster-ip,node-port,loadbalancer,ingress,ingress-controllers/code/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# docker build -t sharadregoti/products:0.1.0 .
+# docker tag sharadregoti/products:0.1.0 sharadregoti/reviews:0.1.0
+# docker tag sharadregoti/products:0.1.0 sharadregoti/details:0.1.0
+# docker tag sharadregoti/products:0.1.0 sharadregoti/ratings:0.1.0
+docker push sharadregoti/products:0.1.0
+# docker push sharadregoti/reviews:0.1.0
+# docker push sharadregoti/details:0.1.0
+# docker push sharadregoti/ratings:0.1.0
